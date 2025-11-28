be280f1cd4c2239a0580a137698def8905b2c62b
skirfan7044 <skirfan7044@gmail.com>
2025-11-28 14:46:21 +0530

    updating

--- js/index.js
+++ js/index.js
@@ -115,7 +115,7 @@ const container = document.getElementById("attraction-container");
 if (container) {
   attractions.forEach((item) => {
     container.innerHTML += `
-       <div class="col-xxl-3 col-md-4 col-sm-6 col-xs-12">
+       <div class="col-xxl-3 col-md-4 col-sm-6 col-12">
         <div class="attraction-card">
           <img src="${item.img}" alt="${item.title}" loading="lazy"/>
           <div class="text-box">
